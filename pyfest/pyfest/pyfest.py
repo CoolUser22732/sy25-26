16fec1f0128b8fb1d3f7d2ddf317fe8fbabfdb1a
KaiWatanabe <kawat339@learner.dvusd.org>
2026-01-09 13:05:55 -0700

    changed

--- pyfest/pyfest/pyfest.py
+++ pyfest/pyfest/pyfest.py
@@ -1,1 +1,16 @@
+# The initial lineup
+lineup = [
+    ("Good Charlotte", "punk rock", 30),
+    ("All American Rejects", "punk rock", 45),
+    ("Jimmy Eat World", "punk rock", 60)
+]
 
+# 1. Add the headliner
+headliner = ("Yellowcard", "punk rock", 90)
+lineup.append(headliner)
+print(lineup)
+remove_band = lineup.pop(0)
+lineup.append(remove_band)
+print(lineup)
+
+# Continue the logic below...
